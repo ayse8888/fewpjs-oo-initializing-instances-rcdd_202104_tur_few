3839481d4fea0d687801f643f7154cefd113ab0b
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-23 18:12:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Write your code here
 class Breakfast {
   constructor(food, drink) {
@@ -23,3 +24,41 @@ class Dinner {
     this._dessert = dessert;
   }
 }
+=======
+// Write your code here
+class Breakfast {
+  constructor(food, juice) {
+    this.food = food;
+    this.juice = juice;
+  }
+}
+let newBreakfast = new Breakfast("pancake", "juice");
+function breakfast() {
+    console.log(`Today's juice is ${this.juice}.`);
+  }
+Breakfast.breakfast()
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+let newLunch = new Lunch("american", "chicken noodle", "wine");
+function lunch() {
+    console.log(`Today's soup is ${this.soup}.`);
+  }
+Lunch.lunch()
+
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this._salad = salad;
+    this._soup = soup;
+    this._entree = entree;
+    this._dessert = dessert;
+  }
+}
+let newDinner = new Dinner("american", "chicken noodle", "grilled chicken", "ice cream");
+>>>>>>> 4f43d3543278ee2aebc4a64b32e840f6340e47fd
